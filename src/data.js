193c3efbe4fb0e7efd7a5a99a8c670ab360e5374
Nikola Perica <np46120@oss.unist.hr>
2023-09-27 12:56:53 +0200

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: "Youtube Search app",
+    subtitle: "Angular",
+    description:
+      "Basic Youtube search and play video",
+    image: "./youtubesearch.PNG",
+    link: "https://youtube-search-app-l6i9.vercel.app/",
+  },
+  {
+    title: "Obiteljska medicina",
+    subtitle: "Bootstrap and PHP",
+    description:
+      "Startup that offers tech support for doctors",
+    image: "./obiteljska.PNG",
+    link: "https://obiteljskamedicina.com",
+  },
+  {
+    title: "Finance manager",
+    subtitle: "Kotlin",
+    description:
+      "Mobile app for tracking finances soon on google store",
+    image: "./financemanager.png",
+    link: "",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "HTML & CSS",
+  "Kotlin",
+  "Material UI",
+];
